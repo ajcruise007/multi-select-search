d32cce94ea9f697b7f59e2b8d820dceeac764b9b
ajcruise007 <ajcruise.007@gmail.com>
2024-10-30 17:01:48 +0530

    multi-select-search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
+import Pill from "./components/Pill";
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [selectedUsers, setSelectedUsers] = useState([]);
+  const [selectedUsersSet, setSelectedUsersSet] = useState(new Set());
+  const inputRef = useRef();
+
+  const handleUserClick = (user) => {
+    inputRef.current.focus();
+    setSelectedUsers([...selectedUsers, user]);
+    setSelectedUsersSet(new Set([...selectedUsersSet, user.email]));
+    setSearchQuery("");
+    setSuggestions([]);
+  };
+
+  const handleRemoveUser = (id) => {
+    const updatedUsersList = selectedUsers.filter((user) => user.id !== id);
+    setSelectedUsers(updatedUsersList);
+
+    // Update in Set
+    const updatedEmails = new Set([updatedUsersList.map((user) => user.email)]);
+    setSelectedUsersSet(updatedEmails);
+  };
+
+  useEffect(() => {
+    if (searchQuery.trim() === "") {
+      setSuggestions([]);
+      return;
+    }
+    fetchUsers(searchQuery);
+  }, [searchQuery]);
+
+  const fetchUsers = async (query) => {
+    try {
+      const res = await fetch(`https://dummyjson.com/users/search?q=${query}`);
+      const data = await res.json();
+      setSuggestions(data?.users);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="user-search-container">
+      <div className="user-search-input">
+        {selectedUsers.map((user) => (
+          <Pill
+            key={user.id}
+            text={`${user.firstName} ${user.lastName}`}
+            image={user.image}
+            onClick={() => handleRemoveUser(user.id)}
+          />
+        ))}
+        <div>
+          <input
+            ref={inputRef}
+            type="text"
+            placeholder="Search for a user..."
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Backspace" && e.target.value === "") {
+                if (selectedUsers.length > 0) {
+                  handleRemoveUser(selectedUsers[selectedUsers.length - 1].id);
+                }
+              }
+            }}
+          />
+        </div>
+      </div>
+      <ul className="suggestions">
+        {suggestions.map((user) => {
+          return (
+            !selectedUsersSet.has(user.email) && (
+              <li key={user.id} onClick={() => handleUserClick(user)}>
+                <img src={user.image} alt={user.firstName} />
+                <span>
+                  {user.firstName} {user.lastName}
+                </span>
+              </li>
+            )
+          );
+        })}
+      </ul>
     </div>
   );
 }
